ed646ae9be183b7aed012f8a5937dd645a051c7d
Slowlyo <1821725017@qq.com>
2023-01-31 23:14:20 +0800

    update to 1.5.5

---
+++ lang/en/menu.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'dashboard'  => 'Dashboard',
+    'admin_system'     => 'Admin',
+    'admin_users'      => 'Users',
+    'admin_roles'      => 'Roles',
+    'admin_permission' => 'Permissions',
+    'admin_menu'       => 'Menus',
+    'admin_setting'    => 'Settings',
+];
