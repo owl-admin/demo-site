f25c83bf7b23bff5c2527ce78a0aa6084ea29424
Slowlyo <slowlyo_email@qq.com>
2023-04-27 13:48:13 +0800

    更新 2.4.9

--- app/Admin/Controllers/CodeGeneratorController.php
+++ app/Admin/Controllers/CodeGeneratorController.php
@@ -10,18 +10,12 @@ class CodeGeneratorController extends \Slowlyo\OwlAdmin\Controllers\DevTools\Cod
     public function index(): \Illuminate\Http\JsonResponse|\Illuminate\Http\Resources\Json\JsonResource
     {
         $page = amisMake()->Page()
-            ->title(__('admin.code_generator'))
             ->body($this->form())
+            ->css($this->css())
             ->toolbar(
                 Components::make()->codeView(
                     base_path('vendor/slowlyo/owl-admin/src/Controllers/DevTools/CodeGeneratorController.php')
                 )
-            )
-            ->remark(
-                __('admin.code_generators.remark1') .
-                "<a href='https://learnku.com/docs/laravel/9.x/migrations/12248#b419dd' target='_blank'>" .
-                __('admin.code_generators.remark2') .
-                "</a>, " . __('admin.code_generators.remark3')
             );
 
         return $this->response()->success($page);
@@ -31,4 +25,55 @@ class CodeGeneratorController extends \Slowlyo\OwlAdmin\Controllers\DevTools\Cod
     {
         return $this->response()->fail('演示站点, 不支持该操作哟');
     }
+
+    private function css()
+    {
+        return [
+            '.cxd-Table-content'                             => ['padding-bottom' => '15px'],
+            '.item-comment'                                  => [
+                'width'         => '220px',
+                'height'        => '18px',
+                'overflow'      => 'hidden',
+                'text-overflow' => 'ellipsis',
+                'white-space'   => 'nowrap',
+                'color'         => '#a9aeb8',
+                'font-size'     => '12px',
+            ],
+            '.column-name'                                   => [
+                'max-width'     => '160px',
+                'overflow'      => 'hidden',
+                'text-overflow' => 'ellipsis',
+                'white-space'   => 'nowrap',
+                'font-weight'   => 'bold',
+            ],
+            '.custom-subform-item'                           => [
+                'border'        => '1px solid #eee',
+                'border-radius' => '4px',
+                'margin'        => '5px',
+                'width'         => '16%',
+                'padding'       => '10px',
+                'min-width'     => '220px',
+                'position'      => 'relative',
+            ],
+            '.custom-subform-item .cxd-SubForm-valueDragBar' => [
+                'position' => 'absolute',
+                'top'      => '5px',
+                'left'     => '10px',
+            ],
+            '.custom-subform-item .cxd-SubForm-valueLabel'   => [
+                'margin-left' => '20px',
+            ],
+            '.custom-subform-item .cxd-SubForm-valueEdit'    => [
+                'position' => 'absolute',
+                'top'      => '5px',
+                'right'    => '30px',
+            ],
+            '.custom-subform-item .cxd-SubForm-valueDel'     => [
+                'position' => 'absolute',
+                'top'      => '5px',
+                'right'    => '10px',
+            ],
+        ];
+    }
 }
+
