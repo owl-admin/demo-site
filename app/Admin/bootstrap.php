0a34703a83183204285c7f732a43db26370e5f00
Slowlyo <slowlyo_email@qq.com>
2023-06-09 15:50:11 +0800

    upgrade v2.6.3

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -1,1 +1,11 @@
 <?php
+
+$msgBtn = amisMake()
+    ->UrlAction()
+    ->icon('fa-brands fa-github')
+    ->blank()
+    ->url('https://github.com/Slowlyo/owl-admin-demo')
+    ->tooltip('demo 源码')
+    ->className('mr-2 rounded-full');
+
+\Slowlyo\OwlAdmin\Admin::prependNav($msgBtn);
