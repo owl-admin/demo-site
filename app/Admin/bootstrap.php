94396028c656b4828591971ac2599cfba89cefb3
Slowlyo <slowlyo_email@qq.com>
2023-06-24 21:28:47 +0800

    menu

--- app/Admin/bootstrap.php
+++ app/Admin/bootstrap.php
@@ -19,18 +19,27 @@ Admin::prependNav($msgBtn);
 // Menu ==========================================================================================
 
 Admin::menu()->add([
-   [
-       'id' => 9998,
-       'title' => '开发示例',
-       'url' => '/dev_example',
-       'icon' => 'vscode-icons:file-type-bolt',
-       'order' => 1
-   ],
-   [
-       'id' => 10001,
-       'parent_id' => 9998,
-       'title' => '左树右表',
-       'url' => '/dev_example/left_tree_right_table',
-       'icon' => 'emojione:evergreen-tree',
-   ]
+    [
+        'id'    => 9998,
+        'title' => '开发示例',
+        'url'   => '/dev_example',
+        'icon'  => 'vscode-icons:file-type-bolt',
+        'order' => 1,
+    ],
+    [
+        'id'        => 10001,
+        'parent_id' => 9998,
+        'title'     => '左树右表',
+        'url'       => '/dev_example/left_tree_right_table',
+        'icon'      => 'emojione:evergreen-tree',
+    ],
+
+    [
+        'id'       => 11001,
+        'title'    => 'AMIS 示例',
+        'url'      => 'https://aisuda.bce.baidu.com/amis/examples/index',
+        'url_type' => 2,
+        'icon'     => 'twemoji:link',
+        'order'    => 2,
+    ],
 ]);
