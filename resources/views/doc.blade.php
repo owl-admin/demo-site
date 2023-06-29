9258c2db310c984d1848063a05d5cbe4bfd67652
Slowlyo <slowlyo_email@qq.com>
2023-06-29 23:31:21 +0800

    站点图标

--- resources/views/doc.blade.php
+++ resources/views/doc.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8"/>
     <title>Owl Admin 中文文档</title>
+    <link rel="icon" href="{{ asset('/admin/logo.png') }}"/>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
     <meta http-equiv="X-UA-Compatible" content="IE=Edge"/>
