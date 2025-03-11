207dc2cdb9fb6622c51ed75840fff609f2045514
slowlyo <slowlyo_email@qq.com>
2025-03-12 00:04:18 +0800

    feat: 调整布局控制器中的网格样式和类名

--- app/Admin/Controllers/Dcat/LayoutController.php
+++ app/Admin/Controllers/Dcat/LayoutController.php
@@ -38,12 +38,12 @@ class LayoutController extends AdminController
                         amis()->GridColumn()->md(12)->body([$card('bg-cyan-300 h-28', 'col-md-12')]),
                     ]),
                     // 一行三列
-                    amis()->Grid()->columns([
+                    amis()->Grid()->className('mt-9')->columns([
                         amis()->GridColumn()->md(4)->body([$card('bg-blue-200 h-full', 'col-md-4')]),
                         amis()->GridColumn()->md(4)->body([$card('bg-blue-200 h-full', 'col-md-4')]),
-                        amis()->GridColumn()->md(4)->body([
+                        amis()->GridColumn()->md(4)->className('h-full')->body([
                             // 一行两列
-                            amis()->Grid()->columns([
+                            amis()->Grid()->className('h-full')->columns([
                                 amis()->GridColumn()->md(6)->body([$card('bg-blue-300 h-full', 'col-md-6')]),
                                 amis()->GridColumn()->md(6)->body([$card('bg-blue-300 h-full', 'col-md-6')]),
                             ]),
